37f20289d48a43e0d0a7c814332b3acc75cb55ab
Liam Malone <maloneliam@proton.me>
2024-09-15 15:07:08 +0100

    glfw opens window and exits on set keygit add .

--- src/glfw-main.c
+++ src/glfw-main.c
@@ -1,10 +1,13 @@
-/* [h] files */
+/* stdlib includes */
 #include <sys/time.h>    // Required for: timespec, nanosleep(), select() - POSIX
 #include <poll.h>        // Required for: ppoll() - POSIX
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
+/* 3rd party library includes */
 #include "external/glad.h"
+#include "external/glad.c"
 
 /* project file includes */
 /* [h] files */
@@ -14,8 +17,27 @@
 #define _GLFW_X11 1
 #include "my_glfw.c"
 
+#ifdef _debug
+/* debug mode defines */
+#define PROGRAM_EXIT_KEY GLFW_KEY_Q
+
+#else
+/* release mode defines */
+#define PROGRAM_EXIT_KEY GLFW_KEY_NULL
+
+#endif
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void input_handle(GLFWwindow* window) {
+    if (glfwGetKey(window, PROGRAM_EXIT_KEY) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+}
+
 int main(int argc, char** argv) {
 
+    int exit_code = 0;
     /* print out program args */
     if (argc > 0) {
         for (int i=0; i<argc; i++) {
@@ -35,16 +57,35 @@ int main(int argc, char** argv) {
 
     if (!window) {
         printf(" :: glfw window init failed ::\n");
-        glfwTerminate();
-        return 1;
+        exit_code = 1;
+        goto cleanup_exit;
     }
 
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        printf("  :: GLAD init failed ::\n");
+        exit_code = 1;
+        goto cleanup_exit;
+    }
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     /* window loop */
+    while (!glfwWindowShouldClose(window))
     {
+        input_handle(window);
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
     }
 
+cleanup_exit:
     glfwTerminate();
-    return 0;
+    return exit_code;
 }
