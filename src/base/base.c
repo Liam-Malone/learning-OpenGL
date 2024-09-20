a5b0f65ba142cc1fa35daa1b52657b2d099da050
Liam Malone <maloneliam@proton.me>
2024-09-20 18:13:09 +0200

    add a single  to include all base_* files

---
+++ src/base/base.c
@@ -0,0 +1,9 @@
+/* A single file to include all the base_* files */
+
+/* [h] files */
+#include "base_types.h"
+#include "base_gfx.h"
+
+/* [c] files */
+#include "base_types.c"
+#include "base_gfx.c"
