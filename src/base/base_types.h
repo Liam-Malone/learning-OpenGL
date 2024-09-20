4e2f4b3b9dcabbfb27ca9ce72a9428c79bcb2b56
Liam Malone <maloneliam@proton.me>
2024-09-20 18:13:58 +0200

    add slice macros, move shader stuff to base_gfx

--- src/base/base_types.h
+++ src/base/base_types.h
@@ -6,25 +6,31 @@
 
 #define global static
 
-/* unsigned int types */
+/* unsigned integer types */
 typedef uint8_t   u8;
 typedef uint16_t  u16;
 typedef uint32_t  u32;
 typedef uint64_t  u64;
 
-/* signed int types */
+/* signed integer types */
 typedef int8_t   i8;
 typedef int16_t  i16;
 typedef int32_t  i32;
 typedef int64_t  i64;
 
-/* float types */
+/* floating point types */
 typedef float f32;
 typedef double f64;
 
+#define slice(T) struct { \
+    T* ptr;               \
+    u64 len;              \
+}
+
 #define dyn_arr(T) struct { \
-    T* arr;                 \
+    T* ptr;                 \
     u64 len;                \
+    u64 cap;                \
 } 
 
 typedef struct Vec4f32 Vec4f32;
@@ -79,35 +85,3 @@ struct Color {
 
 typedef Vec2f32 Velocity;
 typedef Vec3f32 Position;
-
-/* Shader data types */
-typedef struct Shader Shader;
-struct Shader {
-    u32 id;    /* Shader Program ID */
-    i32 *locs; /* Shader Locations Array */
-};
-
-typedef enum ShaderUniformDataType {
-        shader_uniform_f32 = 0,       /* Shader uniform type: float */
-        shader_uniform_vec2f32,            /* Shader uniform type: vec2 (2 float) */
-        shader_uniform_vec3f32,            /* Shader uniform type: vec3 (3 float) */
-        shader_uniform_vec4f32,            /* Shader uniform type: vec4 (4 float) */
-        shader_uniform_i32,             /* Shader uniform type: int */
-        shader_uniform_vec2i32,           /* Shader uniform type: ivec2 (2 int) */
-        shader_uniform_vec3i32,           /* Shader uniform type: ivec3 (3 int) */
-        shader_uniform_vec4i32,           /* Shader uniform type: ivec4 (4 int) */
-        shader_uniform_sampler2d        /* Shader uniform type: sampler2d */
-} ShaderUniformDataType;
-
-typedef enum ShaderAttributeDataType {
-        shader_attribute_f32 = 0,     /* Shader attribute type: float */
-        shader_attribute_vec2f32,          /* Shader attribute type: vec2 (2 float) */
-        shader_attribute_vec3f32,          /* Shader attribute type: vec3 (3 float) */
-        shader_attribute_vec4f32           /* Shader attribute type: vec4 (4 float) */
-} ShaderAttributeDataType;
-
-global Shader Shader_load(const char* restrict vs_filepath, const char* restrict fs_filepath);
-global i32 Shader_get_uniform_location(Shader shader, const char* restrict value);
-global void Shader_set_value(Shader shader, i32 location, void* value, ShaderUniformDataType data_type);
-global void Shader_unload(Shader shader);
-global void Shader_use(Shader shader);
