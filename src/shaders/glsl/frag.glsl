4a576721f6a523176290c27387cca8b37106b3bd
Liam Malone <maloneliam@proton.me>
2024-09-20 13:15:47 +0200

    rgb texture

--- src/shaders/glsl/frag.glsl
+++ src/shaders/glsl/frag.glsl
@@ -8,6 +8,7 @@ in vec2 TexCoord;
 uniform sampler2D ourTexture;
 
 void main() {
-    FragColor = texture(ourTexture, TexCoord);
+    // FragColor = texture(ourTexture, TexCoord);
+    FragColor = texture(ourTexture, TexCoord) * vec4(frag_col, 1.0);
 }
 
